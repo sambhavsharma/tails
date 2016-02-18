55c84c410ca404f9853afc679351d4d0b646ff76
Sambhav Sharma <sambhav.sharma@hotmail.com>
2016-02-19 02:53:18 +0530

    adding code to insert selected tweets in the database

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Small Rails Projects that allows a user/developer to store tweets of a user in d
 
 * Copy config/database.yml.sample to config/database.yml and add respective configuration
 * Copy config/secrets.yml.sample to config/secrets.yml and add respective secret keys
+
+-- To run in development mode:
+  * Copy config/environments/test.rb to config/environments/development.rb and add respective configurations
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,9 +6,6 @@ class TweetsController < ApplicationController
        if params[:username].present?
             username = params[:username]
             options = {:count => 20}
-            # $twitter.user_timeline(username,options).each do |tweet|
-            #     print tweet.text
-            # end
             @tweets = $twitter.user_timeline(username,options)
         end
     end
@@ -16,8 +13,13 @@ class TweetsController < ApplicationController
     def save_tweets
         if params[:tweets].present?
             tweets = params[:tweets]
+ 
+            # ToDo: Bulk/Batch Insert using .create of ActiveRecord
+
             tweets.each do |tweet|
-                print tweet
+                tweet = tweet.split("&&")
+                @tweet = Tweet.new(:tweet_id => tweet[0], :username => tweet[1], :text =>tweet[2])
+                @tweet.save
             end
         end
         render :template => "tweets/index", :locals => {:@message => "Tweets Saved!"}
