11666e435780a1de2f9315b942c64c8b7b9517cd
Sambhav Sharma <sambhav.sharma@hotmail.com>
2016-02-19 00:27:02 +0530

    moving configuration to env specific config and moving them to .gitignore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.consumer_key = '3K77QUvs5oqKHDnxYcXtV1xe5'
+  config.consumer_secret = 'wO8klMys77j6HvXqtVyHNUkA7oAaATjW26EizV7AuG9OwcJqW6'
+  config.access_token = '62075560-Gtb2S9VZ2N5Sw6ixq8pB9BZqxNQmiHD7SQiVsGKSW'
+  config.access_token_secret = 'rEiDZAobc3IS3bNQEZ2daTvs3COmqe3MLOY9jLHmIY9xo'
+  
 end
