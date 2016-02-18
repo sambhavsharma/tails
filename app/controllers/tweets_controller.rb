dea60a51c756d8537c48b6447ebab97860703fc3
Sambhav Sharma <sambhav.sharma@hotmail.com>
2016-02-19 00:11:38 +0530

    adding twitter gem and its configuration

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,11 @@
 class TweetsController < ApplicationController
+
+    #before_filter :load_tweets
+
     def index
+        options = {:count => 20}
+        $twitter.user_timeline("sambhavsharma",options).each do |tweet|
+          puts tweet.text
+        end
     end
 end
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,7 @@
+
+$twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key = '3K77QUvs5oqKHDnxYcXtV1xe5'
+  config.consumer_secret = 'wO8klMys77j6HvXqtVyHNUkA7oAaATjW26EizV7AuG9OwcJqW6'
+  config.access_token = '62075560-Gtb2S9VZ2N5Sw6ixq8pB9BZqxNQmiHD7SQiVsGKSW'
+  config.access_token_secret = 'rEiDZAobc3IS3bNQEZ2daTvs3COmqe3MLOY9jLHmIY9xo'
+end
